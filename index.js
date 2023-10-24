80302949ca116e0fea5d77c6223959c73bfdf39f
Joelle  Johnson <joellej221@gmail.com>
2023-10-24 13:58:26 -0400

    fixed bug

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// Write a function that takes an array of numbers and returns the 3rd highest numbers in the array.
+// Write a function that takes an array of numbers and returns the 3 highest numbers in the array.
 
-const array = [10,43,23,98,45,76,42,76,84]
+const array = [10,43,23,98,45,76,42,75,84]
 
 function return3Largest(arr){
     let largest = arr[0]
     let second = arr[0]
     let third = arr[0]
-    for(let i = 0; i < arr.length; i++){
+    for(let i = 1; i < arr.length; i++){
         if(arr[i] > largest){
             largest = arr[i]
         }
